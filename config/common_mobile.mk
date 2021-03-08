6e642490510580cc0dd379c868abba2198e5cd84
electimon <electimon@gmail.com>
2021-03-08 15:21:51 +0000

    build launcher3 fork
    Change-Id: If88b5635a779218cb488df279cbc4cb9fb1dfe05

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -26,19 +26,11 @@ PRODUCT_PACKAGES += \
     AudioFX
 endif
 
-ifeq ($(PRODUCT_TYPE), go)
 PRODUCT_PACKAGES += \
-    TrebuchetQuickStepGo
+    Launcher3
 
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    TrebuchetQuickStepGo
-else
-PRODUCT_PACKAGES += \
-    TrebuchetQuickStep
-
-PRODUCT_DEXPREOPT_SPEED_APPS += \
-    TrebuchetQuickStep
-endif
+    Launcher3
 
 # Accents
 PRODUCT_PACKAGES += \
